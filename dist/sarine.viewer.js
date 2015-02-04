8271968718d55e1c3a56b3edc47e1f0631d0c00e
ran itzhaki <zamboney@gmail.com>
2015-02-04 12:19:58 +0200

    Commit

---
+++ dist/sarine.viewer.js
@@ -0,0 +1,66 @@
+(function() {
+  var Viewer;
+
+  Viewer = (function() {
+    var error, rm;
+
+    rm = ResourceManager.getInstance();
+
+    function Viewer(options) {
+      this.first_init_defer = $.Deferred();
+      this.full_init_defer = $.Deferred();
+      this.src = options.src, this.element = options.element, this.autoPlay = options.autoPlay;
+      this.id = this.element[0].id;
+      this.element = this.convertElement();
+      Object.getOwnPropertyNames(Viewer.prototype).forEach(function(k) {
+        if (this[k].name === "Error") {
+          return console.error(this.id, k, "Must be implement", this);
+        }
+      }, this);
+      this.element.data("class", this);
+      this.element.on("play", function(e) {
+        return $(e.target).data("class").play.apply($(e.target).data("class"), [true]);
+      });
+      this.element.on("stop", function(e) {
+        return $(e.target).data("class").stop.apply($(e.target).data("class"), [true]);
+      });
+      this.element.on("cancel", function(e) {
+        return $(e.target).data("class").cancel().apply($(e.target).data("class"), [true]);
+      });
+    }
+
+    error = function() {
+      return console.error(this.id, "must be implement");
+    };
+
+    Viewer.prototype.first_init = Error;
+
+    Viewer.prototype.full_init = Error;
+
+    Viewer.prototype.play = Error;
+
+    Viewer.prototype.stop = Error;
+
+    Viewer.prototype.convertElement = Error;
+
+    Viewer.prototype.cancel = function() {
+      return rm.cancel(this);
+    };
+
+    Viewer.prototype.loadImage = function(src) {
+      return rm.loadImage.apply(this, [src]);
+    };
+
+    Viewer.prototype.setTimeout = function(fun, delay) {
+      return rm.setTimeout.apply(this, [this.delay]);
+    };
+
+    return Viewer;
+
+  })();
+
+  console.log("");
+
+  this.Viewer = Viewer;
+
+}).call(this);
