db3feec4dc658f672dc1971e8ca2bfde747543e7
carmeladi <carmeladi@gmail.com>
2015-02-12 16:53:14 +0200

    add copyrights

---
+++ coffee/sarine.viewer.bundle.coffee
@@ -0,0 +1,124 @@
+###!
+sarine.viewer - v0.0.2 -  Thursday, February 12th, 2015, 4:36:10 PM 
+ The source code, name, and look and feel of the software are Copyright © 2015 Sarine Technologies Ltd. All Rights Reserved. You may not duplicate, copy, reuse, sell or otherwise exploit any portion of the code, content or visual design elements without express written permission from Sarine Technologies Ltd. The terms and conditions of the sarine.com website (http://sarine.com/terms-and-conditions/) apply to the access and use of this software.
+###
+
+class Viewer
+  rm = ResourceManager.getInstance();
+  constructor: (options) ->
+    @first_init_defer = $.Deferred()
+    @full_init_defer = $.Deferred()
+    {@src, @element,@autoPlay} = options
+    @id = @element[0].id;
+    @element = @convertElement()
+    Object.getOwnPropertyNames(Viewer.prototype).forEach((k)-> 
+      if @[k].name == "Error" 
+          console.error @id, k, "Must be implement" , @
+    ,
+      @)
+    @element.data "class", @
+    @element.on "play", (e)-> $(e.target).data("class").play.apply($(e.target).data("class"),[true])
+    @element.on "stop", (e)-> $(e.target).data("class").stop.apply($(e.target).data("class"),[true])
+    @element.on "cancel", (e)-> $(e.target).data("class").cancel().apply($(e.target).data("class"),[true])
+  error = () ->
+    console.error(@id,"must be implement" )
+  first_init: Error
+  full_init: Error
+  play: Error
+  stop: Error
+  convertElement : Error
+  cancel : ()-> rm.cancel(@)
+  loadImage : (src)-> rm.loadImage.apply(@,[src])
+  setTimeout : (fun,delay)-> rm.setTimeout.apply(@,[@delay])
+    
+
+      
+console.log ""
+@Viewer = Viewer
+class ResourceManager
+	_instance = undefined
+	_timeoutManager = undefined
+	_imageManger = undefined
+	constructor: ->
+		console.log 'new singleton'
+		_timeoutManager = new TimeoutManager()
+		_imageManger = new ImageManger()
+
+
+	@getInstance: ->
+		if _instance is undefined
+			_instance = new @()
+		_instance
+
+	class ImageManger
+
+		constructor: () ->
+			
+		imageObj : {}
+		loadImage : (src,viewer) ->
+			_t = @
+			if @imageObj[viewer.id] == undefined
+				@imageObj[viewer.id] = {				
+					capacity : viewer.downloadLimit || 2,
+					bag:[],
+					threshhold:[]
+				}
+			defer = $.Deferred()
+			img = new Image()
+			img.crossOrigin = "Anonymous"
+			img.onload = (e) ->
+				index = $.inArray(_t.imageObj[viewer.id].threshhold.filter((v)=> return v.src == e.target.src )[0],_t.imageObj[viewer.id].threshhold)
+				obj = _t.imageObj[viewer.id].threshhold[index]
+				popped = _t.imageObj[viewer.id].bag.shift()
+				if popped
+					popped.img.src = popped.src
+					_t.imageObj[viewer.id].threshhold.push(popped)
+				_t.imageObj[viewer.id].threshhold.splice index , 1
+				obj.defer.resolve(e.target)
+				
+			if @imageObj[viewer.id].threshhold.length < @imageObj[viewer.id].capacity
+				@imageObj[viewer.id].threshhold.push { defer:defer, src:src , img:img}
+				img.src = src
+			else
+				@imageObj[viewer.id].bag.push  { defer:defer, src:src , img:img}
+				
+						
+			
+				
+			# if the copacity is reach the bind to the defer.then(function) of the current downloading image
+			defer
+
+	# @ (this) is for the instance that request the loadImage			
+	loadImage : (src)->  _imageManger.loadImage(src,@)
+
+	class TimeoutManager
+		constructor: () ->
+			# ...
+		funcArr = {}
+
+		@add : (delay,defer,item)->
+			obj = {
+					defer : defer
+					item : item
+				}
+			if(!funcArr[delay])
+				setTimeout((delay)->
+					temp = funcArr[delay]
+					funcArr[delay] = undefined
+					unique = $.unique(temp.map((v)->v.item.id))
+					temp.forEach((i)->i.defer.resolve.apply(i.item))
+				,delay,delay)
+				funcArr[delay] = []
+			if (funcArr[delay].filter((v)->v.item.id == obj.item.id)).length ==  0
+				funcArr[delay].push obj
+
+
+		
+	
+	setTimeout : (delay) ->
+		defer = $.Deferred()
+		TimeoutManager.add(delay,defer,@)
+		defer
+
+@ResourceManager = ResourceManager
+
---
+++ dist/sarine.viewer.bundle.js
@@ -0,0 +1,204 @@
+
+/*!
+sarine.viewer - v0.0.2 -  Thursday, February 12th, 2015, 4:36:10 PM 
+ The source code, name, and look and feel of the software are Copyright © 2015 Sarine Technologies Ltd. All Rights Reserved. You may not duplicate, copy, reuse, sell or otherwise exploit any portion of the code, content or visual design elements without express written permission from Sarine Technologies Ltd. The terms and conditions of the sarine.com website (http://sarine.com/terms-and-conditions/) apply to the access and use of this software.
+ */
+
+(function() {
+  var ResourceManager, Viewer;
+
+  Viewer = (function() {
+    var error, rm;
+
+    rm = ResourceManager.getInstance();
+
+    function Viewer(options) {
+      this.first_init_defer = $.Deferred();
+      this.full_init_defer = $.Deferred();
+      this.src = options.src, this.element = options.element, this.autoPlay = options.autoPlay;
+      this.id = this.element[0].id;
+      this.element = this.convertElement();
+      Object.getOwnPropertyNames(Viewer.prototype).forEach(function(k) {
+        if (this[k].name === "Error") {
+          return console.error(this.id, k, "Must be implement", this);
+        }
+      }, this);
+      this.element.data("class", this);
+      this.element.on("play", function(e) {
+        return $(e.target).data("class").play.apply($(e.target).data("class"), [true]);
+      });
+      this.element.on("stop", function(e) {
+        return $(e.target).data("class").stop.apply($(e.target).data("class"), [true]);
+      });
+      this.element.on("cancel", function(e) {
+        return $(e.target).data("class").cancel().apply($(e.target).data("class"), [true]);
+      });
+    }
+
+    error = function() {
+      return console.error(this.id, "must be implement");
+    };
+
+    Viewer.prototype.first_init = Error;
+
+    Viewer.prototype.full_init = Error;
+
+    Viewer.prototype.play = Error;
+
+    Viewer.prototype.stop = Error;
+
+    Viewer.prototype.convertElement = Error;
+
+    Viewer.prototype.cancel = function() {
+      return rm.cancel(this);
+    };
+
+    Viewer.prototype.loadImage = function(src) {
+      return rm.loadImage.apply(this, [src]);
+    };
+
+    Viewer.prototype.setTimeout = function(fun, delay) {
+      return rm.setTimeout.apply(this, [this.delay]);
+    };
+
+    return Viewer;
+
+  })();
+
+  console.log("");
+
+  this.Viewer = Viewer;
+
+  ResourceManager = (function() {
+    var ImageManger, TimeoutManager, _imageManger, _instance, _timeoutManager;
+
+    _instance = void 0;
+
+    _timeoutManager = void 0;
+
+    _imageManger = void 0;
+
+    function ResourceManager() {
+      console.log('new singleton');
+      _timeoutManager = new TimeoutManager();
+      _imageManger = new ImageManger();
+    }
+
+    ResourceManager.getInstance = function() {
+      if (_instance === void 0) {
+        _instance = new this();
+      }
+      return _instance;
+    };
+
+    ImageManger = (function() {
+      function ImageManger() {}
+
+      ImageManger.prototype.imageObj = {};
+
+      ImageManger.prototype.loadImage = function(src, viewer) {
+        var defer, img, _t;
+        _t = this;
+        if (this.imageObj[viewer.id] === void 0) {
+          this.imageObj[viewer.id] = {
+            capacity: viewer.downloadLimit || 2,
+            bag: [],
+            threshhold: []
+          };
+        }
+        defer = $.Deferred();
+        img = new Image();
+        img.crossOrigin = "Anonymous";
+        img.onload = function(e) {
+          var index, obj, popped;
+          index = $.inArray(_t.imageObj[viewer.id].threshhold.filter((function(_this) {
+            return function(v) {
+              return v.src === e.target.src;
+            };
+          })(this))[0], _t.imageObj[viewer.id].threshhold);
+          obj = _t.imageObj[viewer.id].threshhold[index];
+          popped = _t.imageObj[viewer.id].bag.shift();
+          if (popped) {
+            popped.img.src = popped.src;
+            _t.imageObj[viewer.id].threshhold.push(popped);
+          }
+          _t.imageObj[viewer.id].threshhold.splice(index, 1);
+          return obj.defer.resolve(e.target);
+        };
+        if (this.imageObj[viewer.id].threshhold.length < this.imageObj[viewer.id].capacity) {
+          this.imageObj[viewer.id].threshhold.push({
+            defer: defer,
+            src: src,
+            img: img
+          });
+          img.src = src;
+        } else {
+          this.imageObj[viewer.id].bag.push({
+            defer: defer,
+            src: src,
+            img: img
+          });
+        }
+        return defer;
+      };
+
+      return ImageManger;
+
+    })();
+
+    ResourceManager.prototype.loadImage = function(src) {
+      return _imageManger.loadImage(src, this);
+    };
+
+    TimeoutManager = (function() {
+      var funcArr;
+
+      function TimeoutManager() {}
+
+      funcArr = {};
+
+      TimeoutManager.add = function(delay, defer, item) {
+        var obj;
+        obj = {
+          defer: defer,
+          item: item
+        };
+        if (!funcArr[delay]) {
+          setTimeout(function(delay) {
+            var temp, unique;
+            temp = funcArr[delay];
+            funcArr[delay] = void 0;
+            unique = $.unique(temp.map(function(v) {
+              return v.item.id;
+            }));
+            return temp.forEach(function(i) {
+              return i.defer.resolve.apply(i.item);
+            });
+          }, delay, delay);
+          funcArr[delay] = [];
+        }
+        if ((funcArr[delay].filter(function(v) {
+          return v.item.id === obj.item.id;
+        })).length === 0) {
+          return funcArr[delay].push(obj);
+        }
+      };
+
+      return TimeoutManager;
+
+    })();
+
+    ResourceManager.prototype.setTimeout = function(delay) {
+      var defer;
+      defer = $.Deferred();
+      TimeoutManager.add(delay, defer, this);
+      return defer;
+    };
+
+    return ResourceManager;
+
+  })();
+
+  this.ResourceManager = ResourceManager;
+
+}).call(this);
