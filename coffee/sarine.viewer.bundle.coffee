4c4ad939e86885b8f8570040adb41a07a21d6e2b
SarineColor <github@sarine.com>
2015-04-06 18:18:42 +0300

    deploy 0.1.0

--- coffee/sarine.viewer.bundle.coffee
+++ coffee/sarine.viewer.bundle.coffee
@@ -1,11 +1,12 @@
 ###!
-sarine.viewer - v0.0.13 -  Wednesday, March 25th, 2015, 4:01:39 PM 
+sarine.viewer - v0.1.0 -  Monday, April 6th, 2015, 6:18:28 PM 
  The source code, name, and look and feel of the software are Copyright © 2015 Sarine Technologies Ltd. All Rights Reserved. You may not duplicate, copy, reuse, sell or otherwise exploit any portion of the code, content or visual design elements without express written permission from Sarine Technologies Ltd. The terms and conditions of the sarine.com website (http://sarine.com/terms-and-conditions/) apply to the access and use of this software.
 ###
 
 class Viewer
   rm = ResourceManager.getInstance();
   constructor: (options) ->
+    console.log("")
     @first_init_defer = $.Deferred()
     @full_init_defer = $.Deferred()
     {@src, @element,@autoPlay,@callbackPic} = options
@@ -32,99 +33,4 @@ class Viewer
   setTimeout : (delay,callback)-> rm.setTimeout.apply(@,[@delay,callback]) 
     
 @Viewer = Viewer 
-
-class ResourceManager
-	_instance = undefined
-	_timeoutManager = undefined
-	_imageManger = undefined
-	constructor: ->
-		console.log 'new singleton'
-		_timeoutManager = new TimeoutManager()
-		_imageManger = new ImageManger()
-
-
-	@getInstance: ->
-		if _instance is undefined
-			_instance = new @()
-		_instance
-
-	class ImageManger
-
-		constructor: () ->
-			
-		viewerImagesObj : {}
-		loadImage : (src,viewer,defer) -> 
-			_t = @
-			if @viewerImagesObj[viewer.id] == undefined
-				@viewerImagesObj[viewer.id] = {				
-					capacity : viewer.downloadLimit || 1000,
-					bag:[],
-					threshhold:[],
-					order : parseInt viewer.element.data("order")
-				}
-			defer = defer || $.Deferred()
-			img = new Image()
-			img.crossOrigin = "Anonymous"
-			img.onload = (e) ->
-				index = $.inArray(_t.viewerImagesObj[viewer.id].threshhold.filter((v)=> return v.src == e.target.src )[0],_t.viewerImagesObj[viewer.id].threshhold)
-				obj = _t.viewerImagesObj[viewer.id].threshhold[index]
-				popped = _t.viewerImagesObj[viewer.id].bag.shift()
-				if popped
-					popped.img.src = popped.src
-					_t.viewerImagesObj[viewer.id].threshhold.push(popped)
-				_t.viewerImagesObj[viewer.id].threshhold.splice index , 1
-				obj.defer.resolve(e.target)
-				
-			if @viewerImagesObj[viewer.id].threshhold.length < @viewerImagesObj[viewer.id].capacity
-				@viewerImagesObj[viewer.id].threshhold.push { defer:defer, src:src , img:img}
-				img.src = src
-			else
-				@viewerImagesObj[viewer.id].bag.push  { defer:defer, src:src , img:img}
-				
-			img.onerror = (e) ->				  
-				index = $.inArray(_t.viewerImagesObj[viewer.id].threshhold.filter((v)=> return v.src == e.target.src )[0],_t.viewerImagesObj[viewer.id].threshhold)
-				obj = _t.viewerImagesObj[viewer.id].threshhold[index]
-				if(e.target.src != viewer.callbackPic)
-					_t.loadImage(viewer.callbackPic,viewer,defer)			
-				else
-					throw new Error('callbackPic not exist')
-				
-				
-			# if the copacity is reach the bind to the defer.then(function) of the current downloading image
-			defer
-
-	# @ (this) is for the instance that request the loadImage			
-	loadImage : (src)->  _imageManger.loadImage(src,@)
-
-	class TimeoutManager
-		constructor: () ->
-			# ...
-		funcArr = {}
-
-		@add : (delay,callback,item)->
-			
-			if(!funcArr[delay])
-				setTimeout((delay)->
-					temp = funcArr[delay]
-					funcArr[delay] = undefined
-					unique = $.unique(temp.map((v)->v.item.id))
-					temp.forEach((i)->i.callback.apply(i.item)) 
-				,delay,delay)			
-				funcArr[delay] = []
-			obj = {
-					callback : callback
-					item : item
-				}
-			if (funcArr[delay].filter((v)->v.item.id == obj.item.id)).length ==  0
-				funcArr[delay].push obj
-
- 
-		
-	
-	setTimeout : (delay,callback) ->
-
-		TimeoutManager.add(delay,callback,@)
-		
-
-@ResourceManager = ResourceManager
 
--- dist/sarine.viewer.bundle.js
+++ dist/sarine.viewer.bundle.js
@@ -1,11 +1,11 @@
 
 /*!
-sarine.viewer - v0.0.13 -  Wednesday, March 25th, 2015, 4:01:39 PM 
+sarine.viewer - v0.1.0 -  Monday, April 6th, 2015, 6:18:28 PM 
  The source code, name, and look and feel of the software are Copyright © 2015 Sarine Technologies Ltd. All Rights Reserved. You may not duplicate, copy, reuse, sell or otherwise exploit any portion of the code, content or visual design elements without express written permission from Sarine Technologies Ltd. The terms and conditions of the sarine.com website (http://sarine.com/terms-and-conditions/) apply to the access and use of this software.
  */
 
 (function() {
-  var ResourceManager, Viewer;
+  var Viewer;
 
   Viewer = (function() {
     var error, rm;
@@ -13,6 +13,7 @@ sarine.viewer - v0.0.13 -  Wednesday, March 25th, 2015, 4:01:39 PM
     rm = ResourceManager.getInstance();
 
     function Viewer(options) {
+      console.log("");
       this.first_init_defer = $.Deferred();
       this.full_init_defer = $.Deferred();
       this.src = options.src, this.element = options.element, this.autoPlay = options.autoPlay, this.callbackPic = options.callbackPic;
@@ -66,149 +67,5 @@ sarine.viewer - v0.0.13 -  Wednesday, March 25th, 2015, 4:01:39 PM
   })();
 
   this.Viewer = Viewer;
-
-  ResourceManager = (function() {
-    var ImageManger, TimeoutManager, _imageManger, _instance, _timeoutManager;
-
-    _instance = void 0;
-
-    _timeoutManager = void 0;
-
-    _imageManger = void 0;
-
-    function ResourceManager() {
-      console.log('new singleton');
-      _timeoutManager = new TimeoutManager();
-      _imageManger = new ImageManger();
-    }
-
-    ResourceManager.getInstance = function() {
-      if (_instance === void 0) {
-        _instance = new this();
-      }
-      return _instance;
-    };
-
-    ImageManger = (function() {
-      function ImageManger() {}
-
-      ImageManger.prototype.viewerImagesObj = {};
-
-      ImageManger.prototype.loadImage = function(src, viewer, defer) {
-        var img, _t;
-        _t = this;
-        if (this.viewerImagesObj[viewer.id] === void 0) {
-          this.viewerImagesObj[viewer.id] = {
-            capacity: viewer.downloadLimit || 1000,
-            bag: [],
-            threshhold: [],
-            order: parseInt(viewer.element.data("order"))
-          };
-        }
-        defer = defer || $.Deferred();
-        img = new Image();
-        img.crossOrigin = "Anonymous";
-        img.onload = function(e) {
-          var index, obj, popped;
-          index = $.inArray(_t.viewerImagesObj[viewer.id].threshhold.filter((function(_this) {
-            return function(v) {
-              return v.src === e.target.src;
-            };
-          })(this))[0], _t.viewerImagesObj[viewer.id].threshhold);
-          obj = _t.viewerImagesObj[viewer.id].threshhold[index];
-          popped = _t.viewerImagesObj[viewer.id].bag.shift();
-          if (popped) {
-            popped.img.src = popped.src;
-            _t.viewerImagesObj[viewer.id].threshhold.push(popped);
-          }
-          _t.viewerImagesObj[viewer.id].threshhold.splice(index, 1);
-          return obj.defer.resolve(e.target);
-        };
-        if (this.viewerImagesObj[viewer.id].threshhold.length < this.viewerImagesObj[viewer.id].capacity) {
-          this.viewerImagesObj[viewer.id].threshhold.push({
-            defer: defer,
-            src: src,
-            img: img
-          });
-          img.src = src;
-        } else {
-          this.viewerImagesObj[viewer.id].bag.push({
-            defer: defer,
-            src: src,
-            img: img
-          });
-        }
-        img.onerror = function(e) {
-          var index, obj;
-          index = $.inArray(_t.viewerImagesObj[viewer.id].threshhold.filter((function(_this) {
-            return function(v) {
-              return v.src === e.target.src;
-            };
-          })(this))[0], _t.viewerImagesObj[viewer.id].threshhold);
-          obj = _t.viewerImagesObj[viewer.id].threshhold[index];
-          if (e.target.src !== viewer.callbackPic) {
-            return _t.loadImage(viewer.callbackPic, viewer, defer);
-          } else {
-            throw new Error('callbackPic not exist');
-          }
-        };
-        return defer;
-      };
-
-      return ImageManger;
-
-    })();
-
-    ResourceManager.prototype.loadImage = function(src) {
-      return _imageManger.loadImage(src, this);
-    };
-
-    TimeoutManager = (function() {
-      var funcArr;
-
-      function TimeoutManager() {}
-
-      funcArr = {};
-
-      TimeoutManager.add = function(delay, callback, item) {
-        var obj;
-        if (!funcArr[delay]) {
-          setTimeout(function(delay) {
-            var temp, unique;
-            temp = funcArr[delay];
-            funcArr[delay] = void 0;
-            unique = $.unique(temp.map(function(v) {
-              return v.item.id;
-            }));
-            return temp.forEach(function(i) {
-              return i.callback.apply(i.item);
-            });
-          }, delay, delay);
-          funcArr[delay] = [];
-        }
-        obj = {
-          callback: callback,
-          item: item
-        };
-        if ((funcArr[delay].filter(function(v) {
-          return v.item.id === obj.item.id;
-        })).length === 0) {
-          return funcArr[delay].push(obj);
-        }
-      };
-
-      return TimeoutManager;
-
-    })();
-
-    ResourceManager.prototype.setTimeout = function(delay, callback) {
-      return TimeoutManager.add(delay, callback, this);
-    };
-
-    return ResourceManager;
-
-  })();
-
-  this.ResourceManager = ResourceManager;
 
 }).call(this);
